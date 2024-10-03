40c8ca315da0968d7723719f56d3799e0bc5d83c
Pedro <pedroguilherme.oliveira14@gmail.com>
2024-10-03 02:03:11 -0300

    commit adicionando atualizações aulas de java

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,18 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Tipos primitivos
+        // estudem a classe String que representa texto na aplcação
+
+        String meuNome = "Pedro";
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+
+        //Valor constante sempre com final e CAIXA_ALTA
+        final double VALOR_DE_PI = 3.14;
+        //double VALOR_DE_PI = 10.43;
     }
 }
