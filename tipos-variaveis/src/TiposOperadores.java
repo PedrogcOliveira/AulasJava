df77af4bff3a54ab94c9407c90d7427e26021ad5
Pedro <pedroguilherme.oliveira14@gmail.com>
2024-10-06 15:29:14 -0300

    Finalização de operadores

--- tipos-variaveis/src/TiposOperadores.java
+++ tipos-variaveis/src/TiposOperadores.java
@@ -1,5 +1,47 @@
 public class TiposOperadores {
 
-    
+    public static void main(String[] args) {
+        int numero = 1;
+        int numero1 = 1; 
+        int numero2 = 2;
+        int a = 5;
+        int b = 6;
+        //numero = numero + 2;
+
+        numero = numero ++;
+        System.out.println(numero);
+
+        //Operador ternário
+        String resultado = a==b ?"verdadeiro" : "falso";
+
+        String nomeUm = "Pedro";
+        String nomeDois = new String("Pedro");
+
+        System.out.println(nomeUm == nomeDois);
+        System.out.println(nomeUm.equals(nomeDois));
+
+        //Usando operadores de forma manual
+        boolean simNao = numero1 == numero2;
+
+        System.out.println("NumeroUm é Igual a NumeroDois ?" + simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("NumeroUm é Igual a NumeroDois ?\" + simNao");
+
+        simNao = numero1 > numero2;
+
+        System.out.println("NumeroUm é Igual a NumeroDois ?" + simNao);
+        
+
+        //Usando IF
+        System.out.println("A é igual a B ?");
+
+        if( a > b) {
+            System.out.println("Sim");
+        } else {
+            System.out.println("Não");
+        }
+    }
     
 }
