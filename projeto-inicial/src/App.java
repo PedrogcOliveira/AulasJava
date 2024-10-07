20f30142d03478a36774296dc17dea775645a2b9
Pedro <pedroguilherme.oliveira14@gmail.com>
2024-10-07 03:01:41 -0300

    Realizando a coleta pelo Scanner

--- projeto-inicial/src/App.java
+++ projeto-inicial/src/App.java
@@ -1,5 +1,35 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+
+        System.out.println("Digite sua altura");
+        double altura = scanner.nextDouble();
+
+        System.out.println("Digite seu nome ");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu Sobrenome ");
+        String sobreNome = scanner.next();
+
+        System.out.println("Digite sua idade ");
+        int idade = scanner.nextInt();
+
+
+        //Imprimindo os dados inseridos pelo usuário
+
+        System.out.println("Meu nome é: " + nome + " " + sobreNome);
+        System.out.println("Tenho " + idade + " anos.");
+        System.out.println("Minha altura é " + altura + "Cm.");
+
+        // String nome = args [0];
+        // String sobreNome = args [1];
+        // int idade = Integer.valueOf(args [2]);
+        // double altura = Double.valueOf(args [3]);
+
+
     }
 }
