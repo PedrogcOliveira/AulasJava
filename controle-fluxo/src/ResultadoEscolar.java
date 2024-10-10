7e6b45d48bfebaef14dd53782456756a70ac0df8
Pedro <pedroguilherme.oliveira14@gmail.com>
2024-10-10 01:22:14 -0300

    Finalização módulo controle de fluxos

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -2,13 +2,19 @@ public class ResultadoEscolar {
     public static void main(String[] args) {
         int nota = 6;
 
-        if(nota >= 7) {
-            System.out.println("Aprovado");
-        } else if (nota >= 5 && nota <=7){
-            System.out.println("Prova de Recuperação");
-        }else {
-            System.out.println("Reprovado");
-        }
+        //Controle de fluxo Ternário
+        // String resultado = nota >=7 ? "Aprovado" : nota >=5 && <=7 ? "Prova de recuperação" : "Reprovado";
+        // System.out.println(resultado);
+
+        //Controle de fluxo Condicional
+        //
+        // if(nota >= 7) {
+        //     System.out.println("Aprovado");
+        // } else if (nota >= 5 && nota <=7){
+        //     System.out.println("Prova de Recuperação");
+        // }else {
+        //     System.out.println("Reprovado");
+        // }
     
     }
 }
