e5672072323b9f45b5a84dae8560e16f05cb471a
Pedro <pedroguilherme.oliveira14@gmail.com>
2024-10-11 01:46:02 -0300

    Introduzindo estruturas de repetição

---
+++ estruturas-repeticao/src/EstruturaFor.java
@@ -0,0 +1,20 @@
+public class EstruturaFor {
+    public static void main(String[] args) throws Exception {
+        
+        for(int carneirinhos = 0; carneirinhos <= 20; carneirinhos ++){
+            System.out.println("Contando Carneirinhos" + carneirinhos);
+        }
+        
+        //FOR em ARRAYS
+        // String alunos [] = {"FELIPE", "JONAS", "JULIA", "MARCOS"};
+        // for (int x= 0; x<alunos.length; x++) {
+        //     System.out.println("O aluno no indice x= " + x + "é" + alunos[x]);
+        // }
+
+        //FOR com variável de escopo
+        //
+        //for (String aluno : alunos) {  
+        // System.out.println("Nome do aluno é:" + aluno); 
+        //}
+    }
+}
